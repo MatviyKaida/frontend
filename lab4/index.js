d36185b401d3cb9af69a1940ddca446a446ae370
MatviyKaida <kajdamatvej@gmail.com>
2025-04-10 11:05:35 +0300

    Removed unused modules from index.js and created the report

--- lab4/index.js
+++ lab4/index.js
@@ -1,12 +1,9 @@
 const express = require('express');
-const fs = require('fs');
-const cors = require('cors');
 const path = require('path');
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
-app.use(cors());
 app.use(express.static('public'));
 
 
