f7b10251d97154b7f3c1792b79acdcfaf11050b6
MatviyKaida <kajdamatvej@gmail.com>
2025-05-18 17:41:38 +0300

    Added booking page

---
+++ lab8-10/src/pages/Booking.jsx
@@ -0,0 +1,19 @@
+import { useParams } from "react-router-dom";
+import CinemaHall from "../components/CinemaHall";
+import movies from "../data/movies";
+
+const Booking = () => {
+  const { id } = useParams();
+  const movie = movies.find(m => m.id === parseInt(id));
+
+  if (!movie) return <div>Фільм не знайдено</div>;
+
+  return (
+    <div className="booking">
+      <h2>Бронювання для: {movie.title}</h2>
+      <CinemaHall />
+    </div>
+  );
+};
+
+export default Booking;
