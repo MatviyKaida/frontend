507d0e60007561fa4a239f1036fa7150365a60a7
MatviyKaida <kajdamatvej@gmail.com>
2025-06-04 13:50:47 +0300

    HeroSection was added

---
+++ exam/src/components/HeroSection.jsx
@@ -0,0 +1,10 @@
+const HeroSection = () => (
+  <div className="w3-display-container bgimg-1 parallax" id="home">
+    <div className="w3-display-middle w3-text-white">
+      <h1 className="w3-jumbo">MY WEBSITE</h1>
+      <p>With a parallax effect</p>
+    </div>
+  </div>
+);
+
+export default HeroSection;
