5aca9015340fc73aecc4820c4bbcb0f3e78b6aee
MatviyKaida <kajdamatvej@gmail.com>
2025-06-04 13:56:02 +0300

    Added about section and portfolio section

---
+++ exam/src/components/AboutSection.jsx
@@ -0,0 +1,8 @@
+const AboutSection = () => (
+  <div className="w3-container w3-padding-64 w3-white">
+    <h2>About</h2>
+    <p>Some text about who we are and what we do...</p>
+  </div>
+);
+
+export default AboutSection;
