f7346abd64d45c5205e4e34c6113877798b65638
MatviyKaida <kajdamatvej@gmail.com>
2025-06-04 13:47:13 +0300

    Initialised project

---
+++ exam/src/App.jsx
@@ -0,0 +1,19 @@
+import HeroSection from "./components/HeroSection";
+import AboutSection from "./components/AboutSection";
+import PortfolioSection from "./components/PortfolioSection";
+import ContactSection from "./components/ContactSection";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <HeroSection />
+      <AboutSection />
+      <PortfolioSection />
+      <ContactSection />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ exam/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css'; // Тут міститься стиль для parallax-ефекту
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
