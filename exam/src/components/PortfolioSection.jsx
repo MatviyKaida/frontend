d1568ce3720189e6dcbb6c6104cb35559dd83eb9
MatviyKaida <kajdamatvej@gmail.com>
2025-06-04 16:50:52 +0300

    Edited portfolio section so that there're images which were made by site's host

--- exam/src/components/PortfolioSection.jsx
+++ exam/src/components/PortfolioSection.jsx
@@ -1,17 +1,33 @@
+import React, { useEffect, useState } from 'react';
 import './PortfolioSection.css';
 
-const PortfolioSection = () => (
-  <>
-    <div className="w3-display-container bgimg-2 parallax">
-      <div className="w3-display-middle w3-text-white">
-        <h1>PORTFOLIO</h1>
+const PortfolioSection = () => {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    fetch('/public/images.json') 
+      .then((res) => res.json())
+      .then((data) => setImages(data))
+      .catch((err) => {
+        console.error('Failed to load images:', err);
+      });
+  }, []);
+
+  return (
+    <section className="portfolio-section w3-container w3-padding-64 w3-light-grey" id="portfolio">
+      <h2 className="portfolio-title">My Portfolio</h2>
+      <div className="portfolio-grid">
+        {images.slice(0, 8).map((src, index) => (
+          <div key={index} className="portfolio-item">
+            <img src={src} alt={`Portfolio ${index + 1}`} />
+          </div>
+        ))}
       </div>
-    </div>
-    <div className="portfolio-section w3-container w3-padding-64 w3-white">
-      <h2>My Work</h2>
-      <p>Some text about my work...</p>
-    </div>
-  </>
-);
+      <div className="load-more-container">
+        <button className="load-more-button">Load more</button>
+      </div>
+    </section>
+  );
+};
 
 export default PortfolioSection;
